7d38f25b1cb272b82ef6687268db3fc7b46f73f0
Youssef Elkhayami <youssef@fonk.amsterdam>
2024-09-26 17:59:19 +0200

    break word instead of break all

--- packages/framework/inline/src/components/v-text.ts
+++ packages/framework/inline/src/components/v-text.ts
@@ -11,7 +11,7 @@ export class VText extends LitElement {
 
   @property({ attribute: false })
   accessor styles: StyleInfo = {
-    'word-break': 'break-all',
+    'word-break': 'break-word',
     'white-space': 'pre-wrap',
     'white-space-collapse': 'preserve',
     cursor: 'text',
